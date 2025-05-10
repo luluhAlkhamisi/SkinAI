344a3db0cea35e7faf888e80dd1083cf865366f0
Eman.H <eman.h.alhazmi@gmail.com>
2025-05-10 18:20:19 +0300

    Update SkinAI.py

--- SkinAI.py
+++ SkinAI.py
@@ -182,7 +182,7 @@ if image_data is not None:
     # Show result screen
     st.image(Image.open(image_data), use_column_width=True)
     st.markdown(f"""
-        <div style='background-color:#FFFFFF;padding:25px;border-radius:20px;text-align:center'>
+        <div style='background-color:#FFFFFF;padding:25px;border-radius:15px;text-align:center'>
             <h2 style='color:#FF4444;'>Disease: {predicted_class.upper()}</h2>
             <p style='font-size:25px; color: black;'>Confidence: {confidence:.2f}%</p>
 
